644eec6df45b1735a135fe6d2d77416f989c1c59
Adrian Pad <adrianlpadilla95@gmail.com>
2018-06-27 16:38:14 -0500

    Devise username and name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180626185918) do
+ActiveRecord::Schema.define(version: 20180627211940) do
 
   create_table "tweets", force: :cascade do |t|
     t.integer "user_id"
@@ -33,8 +33,11 @@ ActiveRecord::Schema.define(version: 20180626185918) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@
 3.times do
   User.create(
     email: Faker::Internet.email,
+    username: Faker::Internet.user_name,
     password: '12345678'
   )
 end
