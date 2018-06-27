95c1303923eed9c8b798c20d576537539de2a591
Adrian Pad <adrianlpadilla95@gmail.com>
2018-06-27 12:43:37 -0500

    Show and new form tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,4 +7,27 @@ class TweetsController < ApplicationController
     @tweet = Tweet.new
   end
 
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to tweets_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+    @user = @tweet.user
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(
+      :body,
+      :user_id
+    )
+  end
+
 end
