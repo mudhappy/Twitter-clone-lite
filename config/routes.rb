251b047aa22cde863939cd28ef9f99c0654443a7
Adrian Pad <adrianlpadilla95@gmail.com>
2018-06-27 13:00:38 -0500

    finished tutorial, it was very bad

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show]
   resources :tweets
+
+  # get '/login', to: 'devise/sessions#new', as: :login
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
 end
