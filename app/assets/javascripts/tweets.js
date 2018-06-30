bbeef1676fe5e582c0686c3a15862dad51f2ee90
Adrian Pad <adrianlpadilla95@gmail.com>
2018-06-30 14:42:26 -0500

    Counter Tweet (a little javascript dirty)

---
+++ app/assets/javascripts/tweets.js
@@ -0,0 +1,31 @@
+$(function () {
+  let tweet = $("#new_tweet");
+
+  // If Tweet Form exists
+  if(tweet.length)
+  {
+    let counter = $(".new-tweet__counter");
+    let body = $("#new_tweet textarea");
+    let button_twittea = $("#button-twittea");
+    let body_length = 0;
+    let new_counter = 0;
+    let counter_initial = 280;
+
+    $(body).on('input', function() {
+        body_length = body.val().length;
+        new_counter = counter_initial - body_length; 
+        
+        counter.text(new_counter);
+        if(new_counter < 0)
+        {
+          tweet.addClass("invalid");
+          button_twittea.prop('disabled', true);
+        }else
+        {
+          tweet.removeClass("invalid");
+          button_twittea.prop('disabled', false);
+        }
+    });
+  }
+
+});
