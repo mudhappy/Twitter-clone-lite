406a18b5eead6e7d2878a914ea2c85fd0d9ab5b8
Adrian Pad <adrianlpadilla95@gmail.com>
2018-06-30 23:31:04 -0500

    CSS Hashtag show

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,7 +1,7 @@
 class HashtagsController < ApplicationController
   def show
-    hashtag = Hashtag.find_by!(name: params[:name])
-    @tweets = hashtag.tweets
+    @hashtag = Hashtag.find_by!(name: params[:name])
+    @tweets = @hashtag.tweets
   rescue ActiveRecord::RecordNotFound => e
     render 'users/404', status: :not_found 
   end
