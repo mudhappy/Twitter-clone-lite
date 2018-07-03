0a439a72bff5cea53ce6b8f9a87230ceda1f0f42
Adrian Pad <adrianlpadilla95@gmail.com>
2018-07-02 20:03:40 -0500

    Conflict resolve: Migration user_id

--- db/migrate/20180626180724_create_tweets.rb
+++ db/migrate/20180626180724_create_tweets.rb
@@ -1,7 +1,6 @@
 class CreateTweets < ActiveRecord::Migration[5.1]
   def change
     create_table :tweets do |t|
-      t.references :user, foreign_key: true
       t.text :body
 
       t.timestamps
